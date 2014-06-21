38f7aaf7ccb39048d1c2af8f72228c359bf47d2f
Forest Belton <fbelton@theore.tc>
2014-06-20 20:41:45 -0700

    Add AST types

---
+++ src/AST.hs
@@ -0,0 +1,56 @@
+module AST where
+
+data Program = Program [Definition]
+  deriving (Show)
+
+data Definition = Global String (Maybe Constant) [IVal]
+  | Function String [String] Statement
+  deriving (Show)
+
+data IVal = IConstant Constant
+  | IName String
+  deriving (Show)
+
+data Statement = Auto [(String, Maybe Constant)] Statement
+  | Externs [String] Statement
+  | Label String Statement
+  | Case Constant Statement
+  | Statements [Statement]
+  | If RValue Statement (Maybe Statement)
+  | While RValue Statement
+  | Switch RValue Statement
+  | Goto RValue
+  | Return (Maybe RValue)
+  | StatementVal (Maybe RValue)
+  deriving (Show)
+
+data RValue = RLVal LValue
+  | RConstant Constant
+  | RAssign LValue AssignOp RValue
+  | RPreIncDec IncDec LValue
+  | RPostIncDec LValue IncDec
+  | RUnary UnaryOp RValue
+  | RBinary RValue BinOp RValue
+  | RTernary RValue RValue RValue
+  | RCall RValue [RValue]
+  deriving (Show)
+
+data AssignOp = Assign | AssignWith BinOp
+  deriving (Show)
+
+data IncDec = Inc | Dec
+  deriving (Show)
+
+data UnaryOp = ArithNegate | LogicNegate | AddressOf
+  deriving (Show)
+
+data BinOp = And | Equal | NotEqual | LessThan | LessThanEqual | GreaterThan
+ | GreaterThanEqual | LeftShift | RightShift | Subtract | Add | Modulo
+ | Multiply | Divide
+  deriving (Show)
+
+data LValue = Name String | Deref RValue | Index RValue RValue
+  deriving (Show)
+
+data Constant = CharLit String | StringLit String | NatLit Integer
+  deriving (Show)
--- src/Parser.hs
+++ src/Parser.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
-module Parser (parse, constant, program) where
+module Parser where
+
+import AST
 
 import Control.Applicative
 import Text.Trifecta
@@ -21,63 +23,84 @@ before p q = (:) <$> p <*> q
 term :: CharParsing m => String -> m String
 term s = string s <* spaces
 
+termTo :: CharParsing m => String -> a -> m a
+termTo s v = term s *> pure v
+
 terms :: CharParsing m => [String] -> m String
 terms = choice . map term
 
 -- nonterminals
--- TODO:
---  * remove left recursion
---  * split out terminals/nonterminals
---  * split out large nonterminals into smaller functions
-program = many definition
+program = Program <$> many definition
 
-definition = (name *> optional (brackets constant) *> (commaSep ival) *> semi) <|>
-  (name *> parens (optional $ commaSep name) *> statement)
+definition = try (Global <$> name <*> optional (brackets constant)
+                <*> (commaSep ival <* semi))
+  <|> Function <$> name <*> parens (commaSep name) <*> statement
 
-ival = constant <|> name
+ival = (IConstant <$> constant)
+  <|> (IName <$> name)
 
-statement = (term "auto" *> (commaSep1 $ name *> optional constant) *> semi *> statement) <|>
-  (term "extrn" *> (commaSep1 name) *> semi *> statement) <|>
-  (name *> colon *> statement) <|>
-  (term "case" *> constant *> colon *> statement) <|>
-  ((braces $ many statement) *> pure 'x') <|>
-  (term "if" *> parens rvalue *> statement *> optional (term "else" *> statement) *> pure 'x') <|>
-  (term "while" *> parens rvalue *> statement) <|>
-  (term "switch" *> rvalue *> statement) <|>
-  (term "goto" *> rvalue *> semi) <|>
-  (term "return" *> optional rvalue *> semi) <|>
-  (optional rvalue *> semi)
+statement = autos
+  <|> (Externs <$> (term "extrn" *> commaSep1 name) <*> statement)
+  <|> (Case <$> (term "case" *> constant <* colon) <*> statement)
+  <|> (Statements <$> braces (many statement))
+  <|> (If <$> (term "if" *> parens rvalue) <*> statement
+        <*> optional (term "else" *> statement))
+  <|> (While <$> (term "while" *> parens rvalue) <*> statement)
+  <|> (Switch <$> (term "switch" *> rvalue) <*> statement)
+  <|> (Goto <$> (term "goto" *> rvalue <* semi))
+  <|> (Return <$> (term "return" *> optional rvalue <* semi))
+  <|> try (StatementVal <$> optional rvalue <* semi)
+  <|> (Label <$> (name <* colon) <*> statement)
 
-rvalue = (parens rvalue) <|>
-  lvalue <|>
-  constant <|>
-  (lvalue *> assign *> rvalue) <|>
-  (inc_dec *> lvalue) <|>
-  (lvalue *> inc_dec) <|>
-  (unary *> rvalue) <|>
-  (term "&" *> rvalue) <|>
-  (rvalue *> binary *> rvalue) <|>
-  (rvalue *> term "?" *> rvalue *> colon *> rvalue) <|>
-  (rvalue *> parens (optional $ commaSep rvalue) *> pure "")
+autos = Auto <$> (term "auto" *> 
+  (commaSep1 ((,) <$> name <*> optional constant)) <* semi) <*> statement
 
-assign = term "=" *> optional binary
+-- TODO: Remove left recursion here
+rvalue = (RConstant <$> constant)
+  <|> try (RAssign <$> lvalue <*> assign <*> rvalue)
+  <|> (RPreIncDec <$> inc_dec <*> lvalue)
+  <|> try (RPostIncDec <$> lvalue <*> inc_dec)
+  <|> (RUnary <$> unary <*> rvalue)
+  <|> (RLVal <$> lvalue)
+--  <|> (RBinary <$> rvalue <*> binary <*> rvalue)
+--  <|> (RTernary <$> rvalue <*> rvalue <*> rvalue)
+--  <|> (RCall <$> rvalue <*> parens (commaSep rvalue))
 
-inc_dec = terms ["++", "--"]
+assign = toAssign <$> (string "=" *> optional binary)
+  where toAssign Nothing   = Assign
+        toAssign (Just op) = AssignWith op
 
-unary = terms ["-", "!"]
+inc_dec = (termTo "++" Inc)
+  <|> (termTo "--" Dec)
 
-binary = terms ["&", "==", "!=", "<", "<=", ">", ">=", "<<", ">>", "-", "+",
-  "%", "*", "/"]
+unary = (termTo "-" ArithNegate)
+  <|> (termTo "!" LogicNegate)
+  <|> (termTo "&" AddressOf)
 
-lvalue = name <|>
-  (term "*" *> rvalue) <|>
-  (rvalue *> brackets rvalue)
+binary = (termTo "&" And)
+  <|> (termTo "==" Equal)
+  <|> (termTo "!=" NotEqual)
+  <|> try (termTo "<=" LessThanEqual)
+  <|> try (termTo ">=" GreaterThanEqual)
+  <|> try (termTo "<<" LeftShift)
+  <|> try (termTo ">>" RightShift)
+  <|> (termTo ">"  GreaterThan)
+  <|> (termTo "<"  LessThan)
+  <|> (termTo "-"  Subtract)
+  <|> (termTo "+"  Add)
+  <|> (termTo "%"  Modulo)
+  <|> (termTo "*"  Multiply)
+  <|> (termTo "/"  Divide)
 
-constant =
-  (natural *> pure "") <|>
-  stringLiteral' <|>
-  stringLiteral
+-- TODO: Remove left recursion here
+lvalue = (Name <$> name)
+  <|> (Deref <$> (term "*" *> rvalue))
+--  <|> (Index <$> rvalue <*> brackets rvalue)
 
-name = alpha `before` many alpha_digit
+constant = (NatLit <$> natural)
+  <|> (CharLit <$> stringLiteral')
+  <|> (StringLit <$> stringLiteral)
+
+name = (alpha `before` many alpha_digit) <* spaces
   where alpha       = letter <|> char '_'
         alpha_digit = alpha <|> digit
