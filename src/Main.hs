91471e9b62bcca4039808e1e8236ea8a8a03045a
Forest Belton <forest@homolo.gy>
2017-06-05 23:40:09 -0700

    Add error checking for program arguments

--- src/Main.hs
+++ src/Main.hs
@@ -5,8 +5,11 @@ import System.Environment
 
 main :: IO ()
 main = do
-    (fileName:_) <- getArgs
-    prgm' <- parseProgram fileName
-    case prgm' of
-        Just prgm -> genProgram prgm
-        _ -> return ()
+    args <- getArgs
+    case args of
+        [fileName] -> do
+            prgm' <- parseProgram fileName
+            case prgm' of
+                Just prgm -> genProgram prgm
+                _ -> return ()
+        _ -> putStrLn "usage: b <filename>"
